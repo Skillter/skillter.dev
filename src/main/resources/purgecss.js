3c6a30193c322e246911834cfb51704bf7d5e4fd
Skillter <57605954+Skillter@users.noreply.github.com>
2023-04-04 05:27:08 +0200

    New unused CSS purger library that cleans up 2 times more

--- src/main/resources/purgecss.js
+++ src/main/resources/purgecss.js
@@ -1,58 +1,29 @@
-// const purgecss = require('purgecss')
-// const fs = require('fs');
+const uncss = require('uncss');
+const fs = require('fs');
+
+var files   = ['templates/**/*.html'],
+    options = {
+        banner       : false,
+        // csspath      : '../static/css/',
+        htmlroot     : 'templates',
+        ignore       : ['#added_at_runtime', /test\-[0-9]+/],
+        ignoreSheets : [/fonts.googleapis/],
+        inject       : function(window) { window.document.querySelector('html').classList.add('no-csscalc', 'csscalc'); },
+        jsdom        : {
+            userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)',
+        },
+        media        : ['(min-width: 700px) handheld and (orientation: landscape)'],
+        raw          : 'h1 { color: green }',
+        report       : false,
+        strictSSL    : true,
+        // stylesheets  : ['bulma.min.css'],
+        timeout      : 1000,
+        // uncssrc      : '.uncssrc',
+        userAgent    : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)'
+    };
 
 
-
-// const purgecssConfig = {
-//   content: ['./templates/**/*.html', './static/**/*.js'], // The files to scan for used classes
-//   css: ['./static/css/bulma.min.css'], // The CSS framework file to extract classes from
-// }
-
-// let purge = new purgecss.PurgeCSS
-// const result = purge.extractSelectorsFromFiles(purgecssConfig.content, purgecssConfig.css)
-
-// // Write the new CSS file with only the used classes
-
-// console.log(result)
-
-// // fs.writeFileSync('./static/css/bulma.purged.css', JSON.stringify(result))
-
-
-
-// const { PurgeCSS } = require("postcss-purgecss");
-
-
-// const purgecssConfig = {
-//   content: ['./templates/**/*.html', './static/**/*.js'], // The files to scan for used classes
-//   css: ['./static/css/bulma.min.css'], // The CSS framework file to extract classes from
-//   output: './static/css/bulma.purged.css'
-// }
-
-// async function run() {
-//     const purgeCSSResult = await new PurgeCSS().purge({
-//         content: ['index.html', '**/*.js', '**/*.html', '**/*.vue'],
-//         css: ['static/css/bulma.min.css'],
-//         output: ['static/css/bulma.purged.css']
-//     })
-// }
-// run()
-
-
-
-const cssPurge = require('css-purge');
-
-const OPTIONS = {
-    zero_ignore_declaration: [],
-    zero_units: []
-  };
-
-  //purging a CSS file with HTML and options
-  cssPurge.purgeCSSFiles({}, 'config_css.json');
-
-
-
-
-
-
-
-
+uncss(files, options, function (error, output) {
+  console.log(output);
+  fs.writeFileSync("./static/css/bulma.purged.css", output)
+});
