f0e393bf88e486a81e051e84a17ef4683c332e25
Skillter <57605954+Skillter@users.noreply.github.com>
2023-04-05 21:08:57 +0200

    Initial Init (frontend)

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
+import TodoList from './TodoList';
+
+function App() {
+  return (
+      <TodoList />
+    );
+}
+
+export default App;
