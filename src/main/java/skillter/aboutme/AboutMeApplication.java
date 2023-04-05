7011d0a34ceeab3c7af0ca5c50bde10242fa4d5e
Skillter <57605954+Skillter@users.noreply.github.com>
2023-04-05 06:05:33 +0200

    Create AboutMeApplication.java

---
+++ src/main/java/skillter/aboutme/AboutMeApplication.java
@@ -0,0 +1,14 @@
+package skillter.aboutme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutMeApplication.class, args);
+		System.out.println("Application has started");
+	}
+
+}
