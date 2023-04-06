2218343e22b075a73a8a134e71eb43ce6e2659af
Skillter <57605954+Skillter@users.noreply.github.com>
2023-04-06 17:04:59 +0200

    Main Javascript file

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'static/particles.json', function() {
+    console.log('particles.js config loaded');
+  });
