06fe0ca26bcf0c798880f0fbe0d8682a5748d362
Skillter <57605954+Skillter@users.noreply.github.com>
2025-04-07 02:59:19 +0200

    Reapply "Merge branch 'main' into production"
    This reverts commit b18ebd71d6eb648e3dd514f19ed491ab89ae7788.

---
+++ src/test/java/skillter/aboutme/AboutMeApplicationTests.java
@@ -0,0 +1,13 @@
+package skillter.aboutme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
